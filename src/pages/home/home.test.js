e5108818cbcfb694b2128b4639c7d92e55ba6815
Stephen Key <stephenkey123@gmail.com>
2018-10-11 14:40:10 -0500

    Add init tests.

--- src/pages/home/home.test.js
+++ src/pages/home/home.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './Home';
+import { expect } from 'chai';
+import { mount, configure } from 'enzyme';
+import { Provider } from 'react-redux';
+import store from '../../redux/store'
+import Home from './home';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Home />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+describe('The home page', () => {  
+  it('renders <Home /> component', () => {
+    const wrapper = mount(<Provider store={store}><Home /></Provider>);
+    expect(wrapper.contains(<p className="home">Home</p>)).to.equal(true);
+  });
 });
